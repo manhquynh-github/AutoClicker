d4bd3444725b8e3bf9b60b0b29f140baf6c4ef8d
Chung Mạnh Quỳnh <manhquynh.ms@outlook.com>
2020-03-10 17:22:36 +0700

    Add project files.

---
+++ autoclicker/Form1.Designer.vb
@@ -0,0 +1,90 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+    Me.components = New System.ComponentModel.Container()
+    Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+    Me.Hotkeys = New System.Windows.Forms.Timer(Me.components)
+    Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+    Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+    Me.Button1 = New System.Windows.Forms.Button()
+    Me.SuspendLayout()
+    '
+    'Timer1
+    '
+    Me.Timer1.Interval = 750
+    '
+    'Hotkeys
+    '
+    Me.Hotkeys.Enabled = True
+    Me.Hotkeys.Interval = 1
+    '
+    'Timer2
+    '
+    Me.Timer2.Interval = 1
+    '
+    'CheckBox1
+    '
+    Me.CheckBox1.AutoSize = True
+    Me.CheckBox1.Location = New System.Drawing.Point(13, 13)
+    Me.CheckBox1.Name = "CheckBox1"
+    Me.CheckBox1.Size = New System.Drawing.Size(65, 17)
+    Me.CheckBox1.TabIndex = 0
+    Me.CheckBox1.Text = "Activate"
+    Me.CheckBox1.UseVisualStyleBackColor = True
+    '
+    'Button1
+    '
+    Me.Button1.Location = New System.Drawing.Point(132, 7)
+    Me.Button1.Name = "Button1"
+    Me.Button1.Size = New System.Drawing.Size(23, 23)
+    Me.Button1.TabIndex = 1
+    Me.Button1.Text = "?"
+    Me.Button1.UseVisualStyleBackColor = True
+    '
+    'Form1
+    '
+    Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+    Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+    Me.ClientSize = New System.Drawing.Size(163, 38)
+    Me.Controls.Add(Me.Button1)
+    Me.Controls.Add(Me.CheckBox1)
+    Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+    Me.MaximizeBox = False
+    Me.MinimizeBox = False
+    Me.Name = "Form1"
+    Me.ShowIcon = False
+    Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+    Me.Text = "Auto Clicker"
+    Me.TopMost = True
+    Me.ResumeLayout(False)
+    Me.PerformLayout()
+
+  End Sub
+  Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents Hotkeys As System.Windows.Forms.Timer
+    Friend WithEvents Timer2 As System.Windows.Forms.Timer
+    Friend WithEvents CheckBox1 As System.Windows.Forms.CheckBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+
+End Class
---
+++ autoclicker/Form1.vb
@@ -0,0 +1,70 @@
+﻿Public Class Form1
+  Private Declare Sub mouse_event _
+    Lib "user32" _
+    Alias "mouse_event" _
+    (ByVal dwFlags As Long,
+     ByVal dx As Long,
+     ByVal dy As Long,
+     ByVal cButtons As Long,
+     ByVal dwExtraInfo As Long)
+
+  Private Declare Function GetAsyncKeyState _
+    Lib "user32" _
+    (ByVal vKey As Long) As Integer
+
+  Private Sub Button1_Click(sender As Object, e As EventArgs) _
+    Handles Button1.Click
+
+    MsgBox("To use, check on Activate then:" & vbNewLine &
+      "Hit F1 to click once every 0.75 seconds" & vbNewLine &
+      "Hit F2 to click once every 0.001 seconds" & vbNewLine &
+      "Hit F3 to stop clicking." & vbNewLine & vbNewLine &
+      "Created by https://github.com/manhquynh-github")
+  End Sub
+
+  Private Sub CheckBox1_CheckedChanged(ByVal sender As Object, ByVal e As EventArgs) _
+    Handles CheckBox1.CheckedChanged
+
+    If CheckBox1.Checked = True Then
+      Hotkeys.Start()
+      Me.TopMost = True
+    Else
+      Hotkeys.Stop()
+      Timer1.Stop()
+      Timer2.Stop()
+      Me.TopMost = False
+    End If
+  End Sub
+
+  Private Sub Hotkeys_Tick(ByVal sender As Object, ByVal e As EventArgs) _
+    Handles Hotkeys.Tick
+
+    If GetAsyncKeyState(Keys.F1) Then
+      Timer1.Start()
+      Timer2.Stop()
+    ElseIf GetAsyncKeyState(Keys.F2) Then
+      Timer2.Start()
+      Timer1.Stop()
+    ElseIf GetAsyncKeyState(Keys.F3) Then
+      Timer1.Stop()
+      Timer2.Stop()
+    End If
+  End Sub
+
+  Private Sub PerformClick()
+    mouse_event(&H2, 0, 0, 0, 1) 'Perform mouse down
+    mouse_event(&H4, 0, 0, 0, 1) 'Perform mouse up
+  End Sub
+
+  Private Sub Timer1_Tick(ByVal sender As Object, ByVal e As EventArgs) _
+    Handles Timer1.Tick
+
+    PerformClick()
+  End Sub
+
+  Private Sub Timer2_Tick(ByVal sender As Object, ByVal e As EventArgs) Handles _
+    Timer2.Tick
+
+    PerformClick()
+  End Sub
+End Class
